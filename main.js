ee4d43a49c3bf7964c7bfaf3938927b5d8db7c71
Yana Wilkins <yav9774@gmail.com>
2017-07-15 15:29:11 -0400

    final

--- main.js
+++ main.js
@@ -18,13 +18,16 @@ for (var i = 0; i < customers.results.length; i++) {
   const customer = customers.results[i]
   const item = document.createElement('li')
   item.innerHTML = `
+    <div class= "profile">
+    <img src="${customer.picture.large}">
     <h2>${customer.name.first} ${customer.name.last}</h2>
     <p>${customer.email} </p>
     <p>${customer.location.street}</p>
     <p>${customer.location.city} ${customer.location.state} ${customer.location
     .postcode}</p>
     <p>${customer.phone}</p>
-    <img src="${customer.picture.large}"> `
+    </div>
+     `
 
   list.appendChild(item)
 }
