b12c67ab48135f5bc83ae6b4d7672b68f7bfb975
Yana Wilkins <yav9774@gmail.com>
2017-08-20 10:53:35 -0400

    cleaned up my code

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-//heading here
-//const title = document.createElement('h1')
-//const body = document.querySelector('body')
-//const section1 = document.createElement('section')
-//title.textContent = 'Internal Company Directory'
-//body.appendChild(section1)
-//section1.appendChild(title)
-//customers = objects
-//let main = document.createElement('main')
-//let pic = document.createElement('img')
-//let first = document.createElement('p')
-//let last = document.createElement('p')
-//let email = document.createElement('p')
-//let tel = document.createElement('p')
+
 const list = document.querySelector('ul')
 
 for (var i = 0; i < customers.results.length; i++) {
